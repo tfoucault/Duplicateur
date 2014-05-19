dfdbd052696ec3a3264263f2dba550217e5a59cb
Thibault FOUCAULT <thibault.foucault@gmail.com>
2014-05-19 16:58:16 +0200

    Interface graphique

---
+++ Duplicateur/MainWindow.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace Duplicateur
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.btnExplore = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.btnExplore);
+            this.flowLayoutPanel1.Controls.Add(this.button1);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(12, 12);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(260, 238);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // btnExplore
+            // 
+            this.btnExplore.Location = new System.Drawing.Point(3, 3);
+            this.btnExplore.Name = "btnExplore";
+            this.btnExplore.Size = new System.Drawing.Size(75, 23);
+            this.btnExplore.TabIndex = 0;
+            this.btnExplore.Text = "Open files";
+            this.btnExplore.UseVisualStyleBackColor = true;
+            this.btnExplore.Click += new System.EventHandler(this.onBtnExploreClick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(84, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Open folder";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Name = "MainWindow";
+            this.Text = "Form1";
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button btnExplore;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Duplicateur/MainWindow.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Duplicateur
+{
+    public partial class MainWindow : Form
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void onBtnExploreClick(object sender, EventArgs e)
+        {
+            /*
+            Stream myStream = null;
+            OpenFileDialog fileExplorer = new OpenFileDialog();
+
+            fileExplorer.InitialDirectory = "c:\\";
+            //openFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            //fileExplorer.FilterIndex = 2;
+            fileExplorer.RestoreDirectory = true;
+
+            if (fileExplorer.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    if ((myStream = fileExplorer.OpenFile()) != null)
+                    {
+                        using (myStream)
+                        {
+                            // Insert code to read the stream here.
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                }
+            }
+             */
+            /*
+            FileFolderDialog ffd = new FileFolderDialog();
+
+            if (ffd.ShowDialog() == DialogResult.OK)
+            {
+                MessageBox.Show(ffd.SelectedPath);
+            }
+             */
+
+
+        }
+    }
+}
